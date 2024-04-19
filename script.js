085cbb7d802b620763ead915e6026f038e9cca84
Pawel Kruczek <pkruczek00@gmail.com>
2024-04-20 00:18:24 +0200

    add function to delete tasks

--- script.js
+++ script.js
@@ -1,57 +1,75 @@
 {
-    const tasks = [
-        {
-            content: "pójść do pracy",
-            done: true
-        },
-        {
-            content: "zrobić zakupy"
-        }
-    ]
-
-    const addTask = (taskInput) => {
-        const taskContent = taskInput.value.trim()
-        tasks.push({ content: taskContent })
+  const tasks = [
+    {
+      content: "pójść do pracy",
+      done: true
+    },
+    {
+      content: "zrobić zakupy"
     }
+  ]
 
-    const refreshInput = (taskInput) => {
-        taskInput.value = ""
-        taskInput.focus()
-    }
-    
-    const render = () => {
-        const tasksList = document.querySelector(".js-tasksList")
-        let htmlString = ""
+  const addTask = (taskInput) => {
+    const taskContent = taskInput.value.trim()
+    tasks.push({ content: taskContent })
+  }
 
-        tasks.forEach((task, index) => {
-            htmlString += `
-                <li class="tasksList__item">
-                    <button class="tasksList__button tasksList__button--markDone">✔</button>
-                    <span>${task.content}</span>
-                    <button class="tasksList__button tasksList__button--deleteTask">🗑</button>
-                </li>
-            `
-        });
-        console.log(htmlString)
-        tasksList.innerHTML = htmlString
-    }
+  const deleteTask = (taskIndex) => {
+    tasks.splice(taskIndex, 1)
+    render()
+  }
 
-    const onSubmit = (event) => {
-        const taskInput = document.querySelector(".js-taskContent")
+  const refreshInput = (taskInput) => {
+    taskInput.value = ""
+    taskInput.focus()
+  }
 
-        event.preventDefault();
-        addTask(taskInput)
-        refreshInput(taskInput)
-        
-        render();
-    };
+  const bindTaskEvents = () => {
+    const deleteButtons = document.querySelectorAll(".js-deleteButton")
+
+    deleteButtons.forEach((button, index) => {
+      button.addEventListener("click", () => {
+        deleteTask(index);
+      })
+    })
+  }
+
+  const render = () => {
+
+    const tasksList = document.querySelector(".js-tasksList")
+    let htmlString = ""
+
+    tasks.forEach((task, index) => {
+      htmlString += `
+      <li class="tasksList__item">
+        <button class="tasksList__button tasksList__button--markDone">✔</button>
+        <span>${task.content}</span>
+        <button class="tasksList__button tasksList__button--deleteTask js-deleteButton">🗑</button>
+      </li>
+      `
+    });
+
+    tasksList.innerHTML = htmlString
+
+    bindTaskEvents()
+  }
+
+  const onSubmit = (event) => {
+    const taskInput = document.querySelector(".js-taskContent")
+
+    event.preventDefault();
+    addTask(taskInput)
+    refreshInput(taskInput)
+    render();
+  };
 
 
-    const init = () => {
-        const form = document.querySelector(".js-form");
-        render();
-        form.addEventListener("submit", onSubmit);
-    };
+  const init = () => {
+    render();
 
-    init();
+    const form = document.querySelector(".js-form");
+    form.addEventListener("submit", onSubmit);
+  };
+
+  init();
 }
